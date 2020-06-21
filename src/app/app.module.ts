bd04afa8074002dde119dd56751475fad0d27779
h-github <hamid.am7777@gmail.com>
2020-06-20 17:02:24 -0700

    services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/envent-thumbnail.component';
+import { EventsListComponent } from './events/event-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/event.service';
+import { ToastrService } from './common/toastr.service';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,7 @@ import { NavBarComponent } from './nav/navbar.component';
     EventThumbnailComponent,
     NavBarComponent,
   ],
+  providers: [EventService, ToastrService],
   imports: [BrowserModule],
   bootstrap: [EventsAppComponent],
 })
---
+++ src/app/events/event-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { EventService } from './shared/event.service';
+import { ToastrService } from '../common/toastr.service';
+
+@Component({
+  selector: 'event-list',
+  templateUrl: './events-list.component.html',
+})
+export class EventsListComponent implements OnInit {
+  events: any[];
+  constructor(
+    private eventService: EventService,
+    private toastrService: ToastrService
+  ) {}
+
+  ngOnInit(): void {
+    this.events = this.eventService.getEvents();
+  }
+
+  handleThumbnailClick = (eventName: string) => {
+    console.log(eventName);
+    this.toastrService.success(eventName);
+  };
+}
